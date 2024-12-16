30c75888458af28c80c3210a40ee058fbbbe8386
Adna <adna.mehanovic1@gmail.com>
2024-12-16 20:47:08 +0100

    Add initial project setup

---
+++ libmanage/src/main/java/com/lms/libmanage/LibManageApplication.java
@@ -0,0 +1,12 @@
+package com.lms.libmanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibManageApplication.class, args);
+	}
+}
---
+++ libmanage/src/test/java/com/lms/libmanage/LibManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lms.libmanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibManageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
