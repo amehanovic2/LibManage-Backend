7084d3d2eed4fe3f14b2b589ad868b50bdcabf11
Adna <adna.mehanovic1@gmail.com>
2024-12-23 17:52:54 +0100

    Remove Role entity and repository

--- libmanage/src/main/java/com/lms/libmanage/entity/user/User.java
+++ libmanage/src/main/java/com/lms/libmanage/entity/user/User.java
@@ -5,6 +5,8 @@ import com.lms.libmanage.entity.Token;
 import com.lms.libmanage.enums.Role;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,6 +15,7 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
+@Data
 @Entity
 @Table(name="user")
 public class User implements UserDetails {
@@ -127,79 +130,36 @@ public class User implements UserDetails {
         return id;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
     public String getUuid() {
         return uuid;
-    }
-
-    public void setUuid(String uuid) {
-        this.uuid = uuid;
     }
 
     public String getFirstname() {
         return firstname;
     }
 
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
-    }
-
     public String getLastname() {
         return lastname;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
     }
 
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public String getAddress() {
         return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
     }
 
     public String getPhoneNumber() {
         return phoneNumber;
     }
 
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
     public Role getRole() {
         return role;
-    }
-
-    public void setRole(Role role) {
-        this.role = role;
     }
 
     public List<Token> getUserTokens() {
         return userTokens;
     }
 
-    public void setUserTokens(List<Token> userTokens) {
-        this.userTokens = userTokens;
-    }
 }
--- libmanage/src/main/java/com/lms/libmanage/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.lms.libmanage.repository;
-
-import com.lms.libmanage.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Integer> {
-    Role findByRoleName(String roleName);
-}
